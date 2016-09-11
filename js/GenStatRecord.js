9c4b7a63868e5b6415ca8c5783d6f752fcb8b6f5
mkishine <mkishine@yahoo.com>
2016-09-11 16:14:33 -0400

    Improved validation of parameters

--- js/GenStatRecord.js
+++ js/GenStatRecord.js
@@ -1,5 +1,4 @@
 'use strict';
-var _ = require('lodash');
 
 var GenStatRecord = function(client, appServer, reportType, asofDate, report,
     port, user, host, flags, reqTime, pid, reqNo)
@@ -7,26 +6,57 @@ var GenStatRecord = function(client, appServer, reportType, asofDate, report,
     if ( arguments.length < 12 ) {
         throw new TypeError('GenStatRecord constructor: insufficient number of arguments passed to GenStatRecord constructor');
     }
-    if ( !_.every(arguments, function(a) {return a != null;}) ) {
-        throw new TypeError('GenStatRecord constructor: all arguments have to be defined ');
-    }
     //TODO: make sure that asof_date is a valid epoch
     //TODO: make sure that flags is a positive integer
     //TODO: make sure that req_time is a positive number
     //TODO: make sure that pid is a positive integer
     //TODO: make sure that req_no a positive integer
-    this._client = client;
-    this._appServer = appServer;
-    this._reportType = reportType;
-    this._asofDate = asofDate;
-    this._report = report;
-    this._port = port;
-    this._user = user;
-    this._host = host;
-    this._flags = flags;
-    this._reqTime = reqTime;
-    this._pid = pid;
-    this._reqNo = reqNo;
+
+    if (client != null ) {
+        this._client = client;
+    } else {
+        throw new TypeError('GenStatRecord constructor: client is null');
+    }
+    if ( appServer != null ) {
+        this._appServer = appServer;
+    } else {
+        throw new TypeError('GenStatRecord constructor: appServer is null');
+    }
+    if ( reportType != null ) {
+        this._reportType = reportType;
+    } else {
+        throw new TypeError('GenStatRecord constructor: reportType is null');
+    }
+    if ( asofDate != null ) {
+        this._asofDate = asofDate;
+    } else {
+        throw new TypeError('GenStatRecord constructor: asofDate is null');
+    }
+    if ( host != null ) {
+        this._host = host;
+    } else {
+        throw new TypeError('GenStatRecord constructor: host is null');
+    }
+    if ( reqTime != null ) {
+        this._reqTime = reqTime;
+    } else {
+        throw new TypeError('GenStatRecord constructor: reqTime is null');
+    }
+    if ( pid != null ) {
+        this._pid = pid;
+    } else {
+        throw new TypeError('GenStatRecord constructor: pid is null');
+    }
+    if ( reqNo != null ) {
+        this._reqNo = reqNo;
+    } else {
+        throw new TypeError('GenStatRecord constructor: reqNo is null');
+    }
+    // these parameters have reasonble defaults
+    this._report = report || '';
+    this._port = port || '';
+    this._user = user || '';
+    this._flags = flags || '';
 };
 
 
