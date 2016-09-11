6bc7eb5b1d86fd6c4bd482299960fbc34fd9aec5
mkishine <mkishine@yahoo.com>
2016-09-11 16:16:12 -0400

    Added  tests for mandatory and optional constructor arguments

--- js/GenStatRecord.spec.js
+++ js/GenStatRecord.spec.js
@@ -1,5 +1,17 @@
 'use strict';
 describe('GenStatRecord', function(){
+    var client = 'client',
+        appServer = 'appServer',
+        reportType = 'reportType',
+        asofDate = 'asofDate',
+        report = 'report',
+        port = 'port',
+        user = 'user',
+        host = 'host',
+        flags = 'flags',
+        reqTime = 'reqTime',
+        pid = 'pid',
+        reqNo = 'reqNo';
     var GenStatRecord;
     beforeAll(function() {
         GenStatRecord = require('./GenStatRecord');
@@ -7,28 +19,42 @@ describe('GenStatRecord', function(){
     describe('constructor', function(){
         it('checks number of arguments', function(){
             expect(function() { new this.GenStatRecord(); }).toThrowError(TypeError);
-            expect(function() { new GenStatRecord(1,2,3,4,5,6,7,8,9,10,11); }).toThrowError(TypeError);
-            expect(function() { new GenStatRecord(1,2,3,4,5,6,7,8,9,10,11,12); }).not.toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, report,
+                port, user, host, flags, reqTime, pid); }).toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, report,
+                port, user, host, flags, reqTime, pid, reqNo); }).not.toThrowError(TypeError);
         });
         it('validates arguments', function(){
-            expect(function() { new GenStatRecord(1,null,3,4,5,6,7,8,9,10,11,12); }).toThrowError(TypeError);
-            expect(function() { new GenStatRecord(1,undefined,3,4,5,6,7,8,9,10,11,12); }).toThrowError(TypeError);
+            // client, appServer, reportType, asofDate, host, reqTime pid, and reqNo must be defined
+            expect(function() { new GenStatRecord(null, appServer, reportType, asofDate, report,
+                port, user, host, flags, reqTime, pid, reqNo); }).toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, null, reportType, asofDate, report,
+                port, user, host, flags, reqTime, pid, reqNo); }).toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, null, asofDate, report,
+                port, user, host, flags, reqTime, pid, reqNo); }).toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, null, report,
+                port, user, null, flags, reqTime, pid, reqNo); }).toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, report,
+                port, user, host, flags, null, pid, reqNo); }).toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, report,
+                port, user, host, flags, reqTime, null, reqNo); }).toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, report,
+                port, user, host, flags, reqTime, pid, null); }).toThrowError(TypeError);
+            // report, port, user, and flags do not have to be defined
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, null,
+                port, user, host, flags, reqTime, pid, reqNo); }).not.toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, report,
+                null, user, host, flags, reqTime, pid, reqNo); }).not.toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, report,
+                port, null, host, flags, reqTime, pid, reqNo); }).not.toThrowError(TypeError);
+            expect(function() { new GenStatRecord(client, appServer, reportType, asofDate, report,
+                port, user, host, null, reqTime, pid, reqNo); }).not.toThrowError(TypeError);
+
         });
     });
     describe('has accessors for', function(){
-        var client = 'client',
-            appServer = 'appServer',
-            reportType = 'reportType',
-            asofDate = 'asofDate',
-            report = 'report',
-            port = 'port',
-            user = 'user',
-            host = 'host',
-            flags = 'flags',
-            reqTime = 'reqTime',
-            pid = 'pid',
-            reqNo = 'reqNo',
-            record;
+
+        var record;
         beforeAll(function() {
             record = new GenStatRecord(client, appServer, reportType, asofDate, report,
                 port, user, host, flags, reqTime, pid, reqNo);
